3a90cb934665493a499c4554cff9683be90cbf70
mconstant <85532172+mconstant@users.noreply.github.com>
2023-09-11 20:34:47 +0000

    tools script

--- get_tools.sh
+++ get_tools.sh
@@ -6,6 +6,9 @@ set -o errexit
 set -o pipefail
 IFS=$'\n\t'
 
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
 wget https://github.com/scrtlabs/SecretNetwork/releases/latest/download/secretcli-Linux
 chmod +x secretcli-Linux
 sudo mv secretcli-Linux /usr/local/bin/secretcli
+. ~/.nix-profile/etc/profile.d/nix.sh
+nix-shell
