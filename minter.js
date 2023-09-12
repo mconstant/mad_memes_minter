25c4e1b8a886f19f00a2df54084b077fa397cf90
mconstant <85532172+mconstant@users.noreply.github.com>
2023-09-12 15:07:06 +0000

    updates

--- minter.js
+++ minter.js
@@ -1,5 +1,10 @@
 const { SecretNetworkClient, Wallet, Secp256k1Pen, coins } = require("secretjs");
+const fs = require('fs');
 require('dotenv').config();
+
+const contract_wasm = fs.readFileSync(
+  "./snip721/snip721_reference_impl.wasm.gz"
+);
 
 async function mintNFT() {
   // Your secret network node URL
@@ -17,6 +22,68 @@ async function mintNFT() {
     walletAddress: wallet.address,
     chainId: process.env.SECRET_CHAIN_ID,
   });
+
+  let codeId = 934;
+  let contractCodeHash =
+    "a448595e3a46197776ff966c980d0de770c052c7f1ced1577027906835126bd5";
+  let contractAddress = "secret1xjvnf8fru5xe2x73g6mdfef9zcj00umhvvzqsp";
+
+  let upload_contract = async () => {
+    let tx = await secretjs.tx.compute.storeCode(
+      {
+        sender: wallet.address,
+        wasm_byte_code: contract_wasm,
+        source: "",
+        builder: "",
+      },
+      {
+        gasLimit: 5_000_000,
+      }
+    );
+
+    if (tx.code) throw tx.raw_log;
+
+
+    const codeId = Number(
+      tx.arrayLog.find((log) => log.type === "message" && log.key === "code_id")
+        .value
+    );
+  
+    console.log("codeId: ", codeId);
+  
+    const contractCodeHash = (
+      await secretjs.query.compute.codeHashByCodeId({ code_id: codeId })
+    ).code_hash;
+    console.log(`Contract hash: ${contractCodeHash}`);
+  };
+
+  upload_contract();
+  
+  let instantiate_contract = async () => {
+    // Create an instance of the Counter contract, providing a starting count
+    const initMsg = {};
+    let tx = await secretjs.tx.compute.instantiateContract(
+      {
+        code_id: codeId,
+        sender: wallet.address,
+        code_hash: contractCodeHash,
+        init_msg: initMsg,
+        label: "Secret Millionaire" + Math.ceil(Math.random() * 10000),
+      },
+      {
+        gasLimit: 400_000,
+      }
+    );
+  
+    //Find the contract_address in the logs
+    const contractAddress = tx.arrayLog.find(
+      (log) => log.type === "message" && log.key === "contract_address"
+    ).value;
+  
+    console.log(contractAddress);
+  };
+  
+  instantiate_contract();
 
 
   const addMinterMsg = new MsgExecuteContract({
@@ -60,6 +127,8 @@ async function mintNFT() {
     const tx = await secretjs.tx.broadcast([addMinterMsg, mintMsg], {
       gasLimit: 5_000_000,
     });
+
+    if (tx.code) throw tx.raw_log;
 
     console.log("Transaction Hash:", tx.transactionHash);
     console.log("Minting NFT successful!");
